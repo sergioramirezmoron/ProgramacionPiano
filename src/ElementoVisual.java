3508d71ba74ee6cf8f56a7ef2d0a3dff9c7e3879
sergioramirezmoron <sergiothd50@gmail.com>
2025-04-03 18:10:10 +0200

    Añadir ElementoVisual

---
+++ src/ElementoVisual.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public interface ElementoVisual extends Medible {
+    public abstract void setPosicion(int x, int y);
+
+    public abstract void setGraphics(Graphics g);
+
+    public abstract void dibujar();
+}
