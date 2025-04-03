90f09f780ae96b941475383d5360c0804a2a1026
sergioramirezmoron <sergiothd50@gmail.com>
2025-04-03 18:14:58 +0200

    Añadir Pulsable

---
+++ src/Pulsable.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+public interface Pulsable extends ElementoVisual {
+    public abstract void pulsar();
+
+    public abstract void soltar();
+
+    public abstract boolean estaPulsado();
+
+    public abstract void setColorPulsado();
+
+    public abstract Color getColorPulsado();
+
+    public abstract Color getColorNoPulsado();
+
+    public default Color getColor() {
+        Color color = null;
+        if (this.estaPulsado()) {
+            color = this.getColorPulsado();
+        } else {
+            color = this.getColorNoPulsado();
+        }
+        return color;
+    }
+}
