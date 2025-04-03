b26dbf179395c9eff7b57eaf3ac86c2f6e57a427
sergioramirezmoron <sergiothd50@gmail.com>
2025-04-03 19:45:37 +0200

    Añadir TeclaBlanca (sin programar cosas)

---
+++ src/jd/piano/teclas/TeclaBlanca.java
@@ -0,0 +1,36 @@
+package jd.piano.teclas;
+
+import java.awt.*;
+
+public class TeclaBlanca extends Tecla{
+    public static final int ANCHURA = 25;
+    public static final int ALTURA = 100;
+    TeclaBlanca(int n) {
+        super(n);
+    }
+
+    @Override
+    public Color getColorNoPulsado() {
+        return Color.WHITE;
+    }
+
+    @Override
+    public int getAltura() {
+        return this.ALTURA;
+    }
+
+    @Override
+    public int getAnchura() {
+        return this.ANCHURA;
+    }
+
+    @Override
+    protected int[] getVerticesX() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+
+    @Override
+    protected int[] getVerticesY() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+}
