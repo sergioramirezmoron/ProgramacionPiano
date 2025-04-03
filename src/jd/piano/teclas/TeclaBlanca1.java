1bfac3579a28c6682fc885b919dc7a07b965d690
sergioramirezmoron <sergiothd50@gmail.com>
2025-04-03 19:47:58 +0200

    Añadir TeclaBlanca1,2,3 (sin programar)

---
+++ src/jd/piano/teclas/TeclaBlanca1.java
@@ -0,0 +1,18 @@
+package jd.piano.teclas;
+
+public class TeclaBlanca1 extends TeclaBlanca {
+    TeclaBlanca1(int n) {
+        super(n);
+        throw new UnsupportedOperationException("Sin programar");
+    }
+
+    @Override
+    protected int[] getVerticesX() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+
+    @Override
+    protected int[] getVerticesY() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+}
---
+++ src/jd/piano/teclas/TeclaBlanca3.java
@@ -0,0 +1,18 @@
+package jd.piano.teclas;
+
+public class TeclaBlanca3 extends TeclaBlanca{
+    TeclaBlanca3(int n) {
+        super(n);
+        throw new UnsupportedOperationException("Sin programar");
+    }
+
+    @Override
+    protected int[] getVerticesX() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+
+    @Override
+    protected int[] getVerticesY() {
+        throw new UnsupportedOperationException("Sin programar");
+    }
+}
