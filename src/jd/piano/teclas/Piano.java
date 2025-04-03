015078415f28bb8cd678167541145d6724a2f568
sergioramirezmoron <sergiothd50@gmail.com>
2025-04-03 20:49:14 +0200

    Correcion Piano

--- src/jd/piano/teclas/Piano.java
+++ src/jd/piano/teclas/Piano.java
@@ -3,21 +3,28 @@ package jd.piano.teclas;
 import javax.swing.text.Position;
 import java.awt.*;
 
-public abstract class Piano {
-   private int teclaFinal;
-   private int teclaInicial;
-   private Position posicion;
-   private Graphics graphics;
+public abstract class Piano implements ElementoVisual {
+    private int teclaFinal;
+    private int teclaInicial;
+    private Position posicion;
+    private Graphics graphics;
 
-   public Piano(){
-      this.teclaInicial = 0;
-      this.teclaFinal = 0;
-      this.posicion = null;
-      this.graphics = null;
-   }
-   public abstract Tecla getTecla(int canal, int nota);
-   public int getTeclaFinal() {return teclaFinal;}
-   public int getTeclaInicial() {return teclaInicial;}
+    public Piano() {
+        this.teclaInicial = 0;
+        this.teclaFinal = 0;
+        this.posicion = null;
+        this.graphics = null;
+    }
+
+    public abstract Tecla getTecla(int canal, int nota);
+
+    public int getTeclaFinal() {
+        return teclaFinal;
+    }
+
+    public int getTeclaInicial() {
+        return teclaInicial;
+    }
 }
 
 
